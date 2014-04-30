7f560fc5b9697f3b35c5b0fdaec074c798e95ce0
David White <david.white@spry-soft.com>
2014-04-30 15:32:50 +0100

    Added BBC agreed legal notice and refactored the module.

--- src/bbc-google-maps-util.js
+++ src/bbc-google-maps-util.js
@@ -1,25 +1,60 @@
-var BBCGoogleMapsUtil = {};
+window.BBCGoogleMapsUtil = (function () {
+  var BBCGoogleMapsUtil = {
 
-/**
- * Prepare the Google map instance with some BBC agreed settings
- *
- * @param {google.maps.Map} map
- */
-BBCGoogleMapsUtil.prepareMap = function (map) {
-  if (map instanceof google.maps.Map === false) {
-    throw new TypeError("argument `map` must be of type `google.maps.Map`");
-  }
-  this.map = map;
-  this.updateAttribution();
-};
+    /**
+     * Prepare the Google map instance with some BBC agreed settings
+     *
+     * @param {google.maps.Map} map
+     */
+    prepareMap: function (map) {
+      if (map instanceof google.maps.Map === false) {
+        throw new TypeError("argument `map` must be of type `google.maps.Map`");
+      }
+      this.map = map;
+      this.updateAttribution();
+      this.addLegalNotices();
+    },
+
+    /**
+     * Update the Google logo with a smaller BBC version
+     *
+     */
+    updateAttribution: function () {
+      var attributionControl = document.createElement('div');
+      attributionControl.className = 'bbcGoogleAttribution';
+      this.map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(attributionControl);
+    },
+
+    /**
+     * Update the Google terms of use link
+     *
+     */
+    addLegalNotices: function () {
+      var legalNotices = document.createElement('div');
+      legalNotices.className = 'bbcGoogleLegalNotices';
+      this.map.controls[google.maps.ControlPosition.BOTTOM_RIGHT].push(legalNotices);
 
 
-BBCGoogleMapsUtil.updateAttribution = function () {
-  var attributionControl = document.createElement('div');
-  attributionControl.className = 'bbcGoogleAttribution';
-  this.map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(attributionControl);
-};
+      google.maps.event.addListener(this.map, "mapdataproviders_changed", function () {
+        var dataProviders = this.get('mapDataProviders');
+        if (dataProviders) {
+          dataProviders += ' - ';
+        }
+        setMarkup(legalNotices, dataProviders);
+      });
 
+      setMarkup(legalNotices);
+    }
+  };
+
+  var setMarkup = function(dom, withHtml) {
+    var googleTermsURL = "http://www.google.com/help/terms_maps_no_navigation.html";
+    dom.innerHTML = (withHtml || '') + '<a href="' + googleTermsURL + '" target="_blank">Terms of use</a>';
+  };
+
+  return BBCGoogleMapsUtil;
+
+})();
 
 if (typeof module !== "undefined" && module !== null) {
   module.exports = BBCGoogleMapsUtil;
