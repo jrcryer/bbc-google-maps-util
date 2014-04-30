bbf2872def882a0d427952b0aa6af2895b3fb24f
David White <david.white@spry-soft.com>
2014-04-30 12:25:21 +0100

    Added BBCified Google attribution logo.

--- src/bbc-google-maps-util.js
+++ src/bbc-google-maps-util.js
@@ -6,9 +6,19 @@ var BBCGoogleMapsUtil = {};
  * @param {google.maps.Map} map
  */
 BBCGoogleMapsUtil.prepareMap = function (map) {
+  if (map instanceof google.maps.Map === false) {
+    throw new TypeError("argument `map` must be of type `google.maps.Map`");
+  }
   this.map = map;
+  this.updateAttribution();
 };
 
+
+BBCGoogleMapsUtil.updateAttribution = function () {
+  var attributionControl = document.createElement('div');
+  attributionControl.className = 'bbcGoogleAttribution';
+  this.map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(attributionControl);
+};
 
 
 if (typeof module !== "undefined" && module !== null) {
--- test/bbc-google-maps-util-test.js
+++ test/bbc-google-maps-util-test.js
@@ -2,7 +2,11 @@ var map;
 
 module( "BBCGoogleMapsUtil", {
   setup: function () {
-    // Setup a Gooogle map instance
+    map = new google.maps.Map(document.getElementById("map-canvas"), {
+      center: new google.maps.LatLng(-34.397, 150.644),
+      zoom: 8,
+      hideLegalNotices: true
+    });
   }, teardown: function () {
     // Destroy Google maps instance
   }
@@ -10,4 +14,21 @@ module( "BBCGoogleMapsUtil", {
 
 test("should define method named prepareMap", function () {
   ok(typeof BBCGoogleMapsUtil.prepareMap === 'function', "BBCGoogleMapsUtil.prepareMap should be a method");
+});
+
+test("should throw error if instance of google.maps.Map is not passed to prepareMap", function () {
+  raises(
+    function () { BBCGoogleMapsUtil.prepareMap({}); },
+    TypeError,
+    "raised error message contains 'description'"
+  );
+});
+
+asyncTest("should add Google attribution to map", function () {
+  google.maps.event.addListenerOnce(map, 'idle', function () {
+    var mapUtil = BBCGoogleMapsUtil.prepareMap(map);
+    var attribution = document.getElementsByClassName('bbcGoogleAttribution');
+    equal(attribution.length, 1, "Should add bbcGoogleAttribution to DOM");
+    start();
+  });
 });
